cbe05f8a9aca304a3ed8a59fbbedf9bf1b1afac6
CJBaquiran <christian.johnathan.baquiran@adamson.edu.ph>
2017-10-17 04:41:54 +0800

    Added Home, Upload pages. Home doesnt have Functionality. Upload shows the upload image but does not have functionality

---
+++ uploadulam/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+	return render(request, 'uploadulam/home.html')
+	
+def upload(request):
+	return render(request, 'uploadulam/upload.html', {'upload':['Call me','09123456789']})
